1f98e1e3f40d2ce144612db82cda942ea876aaf7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-07-21 14:59:27 +0000

    Update variables aux left and right to value NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,6 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *aux = NULL;
 
 	aux = malloc(sizeof(binary_tree_t));
+
 	if (aux == NULL)
 		return (NULL);
 
@@ -19,6 +20,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		aux->parent = NULL;
 	else
 		aux->parent = parent;
+
+	aux->left = NULL;
+	aux->right = NULL;
+
 	aux->n = value;
 	return (aux);
 }
